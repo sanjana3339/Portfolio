1711afd2932f856563eb547398df9ef6a9fb35bd
sanjana3339 <sanjana.mudimala@yahoo.com>
2024-08-12 02:15:55 +0530

    sidebar done

--- frontend/src/components/layout/layout.js
+++ frontend/src/components/layout/layout.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import Home from '../../pages/home/home';
+import Menu from '../menus/menus';
 import { AiOutlineDoubleLeft } from "react-icons/ai";
 import { AiOutlineDoubleRight } from "react-icons/ai";
 import './layout.css'
@@ -14,6 +15,7 @@ return(
     <div>
         <div className='sidebar-section'>
             <div className={toggle?'sidebar-toggle':'sidebar'}>
+            
                 <div className='sidebar-toggle-icons'>
                     <p onClick={handleToggle}>
                         {toggle?
@@ -22,6 +24,7 @@ return(
                     
                     </p>
                 </div>
+                <Menu toggle={toggle}/>
             </div>
             <div className='container'>
                 <Home/>
---
+++ frontend/src/components/menus/menus.js
@@ -0,0 +1,145 @@
+import React from 'react';
+import "./menus.css";
+import { FaInfoCircle } from "react-icons/fa";
+import { IoSchool } from "react-icons/io5";
+import { FaTools } from "react-icons/fa";
+import { FaLaptopCode } from "react-icons/fa";
+import { MdOutlineWork } from "react-icons/md";
+import { PiCertificateBold } from "react-icons/pi";
+import { RiMedalLine } from "react-icons/ri";
+import { RiContactsBook3Fill } from "react-icons/ri";
+import { RiTeamLine } from "react-icons/ri";
+
+function Menus(props)
+{
+return(
+    <div>
+    {
+        props.toggle?
+        (<div className='menu-page-profile-pic'>
+            <img src="https://photosnow.org/wp-content/uploads/2024/04/no-dp-mood-off_9.jpg"/>
+        </div>):
+        (
+        <div className='menu-page-profile-pic-toggle'>
+            <img src="https://photosnow.org/wp-content/uploads/2024/04/no-dp-mood-off_9.jpg"/>
+        </div>
+        )
+    }
+    <div className='nav-items'>
+    {
+        props.toggle?
+        (
+        <><div className='nav-item'>
+            <div className='nav-link'>
+            <FaInfoCircle/>
+            About
+            </div>
+        </div>
+        <div className='nav-item'>
+            <div className='nav-link'>
+            <IoSchool/>
+            Education
+            </div>
+        </div>
+        <div className='nav-item'>
+            <div className='nav-link'>
+            <FaTools/>
+            Skills
+            </div>
+        </div>
+        <div className='nav-item'>
+            <div className='nav-link'>
+            <FaLaptopCode/>
+            Projects
+            </div>
+        </div>
+        <div className='nav-item'>
+            <div className='nav-link'>
+            <MdOutlineWork/>
+            Internship
+            </div>
+        </div>
+        <div className='nav-item'>
+            <div className='nav-link'>
+            <PiCertificateBold/>
+            Certifications
+            </div>
+        </div>
+        <div className='nav-item'>
+            <div className='nav-link'>
+            <RiMedalLine/>
+            Achievements
+            </div>
+        </div>
+        <div className='nav-item'>
+            <div className='nav-link'>
+            <RiTeamLine/>
+            Positions
+            </div>
+        </div>
+        <div className='nav-item'>
+            <div className='nav-link'>
+            <RiContactsBook3Fill/>
+            Contact
+            </div>
+        </div>
+        </>):
+        (
+        <>
+        <div className='nav-item-toggle'>
+            <div className='nav-link'>
+            <FaInfoCircle/>
+            </div>
+        </div>
+        <div className='nav-item-toggle'>
+            <div className='nav-link'>
+            <IoSchool/>
+            </div>
+        </div>
+        <div className='nav-item-toggle'>
+            <div className='nav-link'>
+            <FaTools/>
+            </div>
+        </div>
+        <div className='nav-item-toggle'>
+            <div className='nav-link'>
+            <FaLaptopCode/>
+            </div>
+        </div>
+        <div className='nav-item-toggle'>
+            <div className='nav-link'>
+            <MdOutlineWork/>
+            </div>
+        </div>
+        <div className='nav-item-toggle'>
+            <div className='nav-link'>
+            <PiCertificateBold/>
+            </div>
+        </div>
+        <div className='nav-item-toggle'>
+            <div className='nav-link'>
+            <RiMedalLine/>
+            </div>
+        </div>
+        <div className='nav-item-toggle'>
+            <div className='nav-link'>
+            <RiTeamLine/>
+            </div>
+        </div>
+        <div className='nav-item-toggle'>
+            <div className='nav-link'>
+            <RiContactsBook3Fill/>
+            </div>
+        </div>
+        </>
+        )
+
+    }
+        
+    </div>
+    </div>
+)
+
+}
+
+export default Menus;
