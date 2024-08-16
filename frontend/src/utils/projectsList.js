cd899c1eb222f6d420c6abe9cb450d791d6cbd1e
sanjana3339 <sanjana.mudimala@yahoo.com>
2024-08-17 04:33:11 +0530

    added plist file

---
+++ frontend/src/utils/projectsList.js
@@ -0,0 +1,103 @@
+
+export const projectsList = [
+  {
+    _id: 1,
+    name: "HTML",
+    icon: BsFiletypeHtml,
+  },
+  {
+    _id: 2,
+    name: "CSS",
+    icon: BsFiletypeCss,
+  },
+  {
+    _id: 3,
+    name: "Bootstrap",
+    icon: BsBootstrap,
+  },
+  {
+    _id: 4,
+    name: "Javascript",
+    icon: SiJavascript,
+  },
+  {
+    _id: 5,
+    name: "C",
+    icon: SiC,
+  },
+  {
+    _id: 6,
+    name: "Python",
+    icon: SiPython,
+  },
+  {
+    _id: 7,
+    name: "Java",
+    icon: RiTeamLine,
+  },
+  {
+    _id: 8,
+    name: "TypeScript",
+    icon: SiTypescript,
+  },
+  {
+    _id: 9,
+    name: "AngularJS",
+    icon: SiAngular,
+  },
+  {
+    _id: 10,
+    name: "SpringBoot",
+    icon: SiSpringboot,
+  },
+  {
+    _id: 11,
+    name: "ReactJS",
+    icon: SiReact,
+  },
+  {
+    _id: 12,
+    name: "NodeJS",
+    icon: FaNodeJs,
+  },
+  {
+    _id: 13,
+    name: "ExpressJS",
+    icon: SiExpress,
+  },
+  {
+    _id: 14,
+    name: "Mongodb",
+    icon: SiMongodb,
+  },
+  {
+    _id: 15,
+    name: "MySQL",
+    icon: SiMysql,
+  },
+  {
+    _id: 16,
+    name: "Git-Github",
+    icon: SiGithub,
+  },
+  {
+    _id: 17,
+    name: "TeamWork",
+    icon: SiTeamviewer, // Updated icon for Team Work
+  },
+  {
+    _id: 18,
+    name: "Leadership",
+    icon: RiTeamLine, // Updated icon for Leadership
+  },
+  {
+    _id: 19,
+    name: "Communication",
+    icon: RiTeamLine, // Updated icon for Communication
+  },
+  {
+    _id: 20,
+    name: "Adaptability",
+    icon: RiTeamLine, // Updated icon for Adaptability
+  }
+];
